75bc911cb99bd69204ffea3a5bd2719eee127264
Tom Malone <tom.malone@icloud.com>
2026-04-26 15:40:19 +0100

    Adding jetbrains mono

--- macbook-pro/flake.nix
+++ macbook-pro/flake.nix
@@ -19,6 +19,10 @@
           pkgs.neovim
         ];
 
+      fonts.packages = [
+        (pkgs.nerdfonts.override {fonts = ["JetBrains Mono"];})
+      ];
+
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
 
